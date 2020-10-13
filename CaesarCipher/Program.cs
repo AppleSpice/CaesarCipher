ccac6313a34a1b82afbe31eeab3575591a1d7a8a
Gaven Rudiak <gavenrudiak@outlook.com>
2020-10-13 08:30:10 -0400

    Finished

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -9,10 +9,22 @@ namespace CaesarCipher
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
             Console.WriteLine("Enter your secret word!");
-            string secretWord = Console.ReadLine();
+            string input = Console.ReadLine();
+            char[] secretMessage = input.ToCharArray();
 
-            char[] secretMessage = secretWord.ToCharArray();
             char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int letterPosition = Array.IndexOf(alphabet, letter);
+                int newLetterPosition = (letterPosition + 3) % 26;
+                char letterEncoded = alphabet[newLetterPosition];
+                encryptedMessage[i] = letterEncoded;
+            }
+
+            string encodedString = string.Join("", encryptedMessage);
+            Console.WriteLine($"Your encoded message is: {encodedString}");
         }
     }
 }
