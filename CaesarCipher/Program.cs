c094fc7e57804a806db5279bec68a33013e98cfe
Gaven Rudiak <gavenrudiak@outlook.com>
2020-10-13 08:08:45 -0400

    Add project files.

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("Enter your secret word!");
+            string secretWord = Console.ReadLine();
+
+            char[] secretMessage = secretWord.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+        }
+    }
+}
